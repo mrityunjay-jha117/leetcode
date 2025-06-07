7aeed84d1e8847264cb62c101cc0d7c17ffa85b6
mrityunjay jha <158163203+mrityunjay-jha117@users.noreply.github.com>
2025-06-07 13:42:33 +0530

    Time: 55 ms (82.47%), Space: 35.2 MB (36.69%) - LeetHub

---
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string clearStars(string s) {
+        int n=s.size();
+        vector<vector<int>>buckets(26);
+        vector<int>removed(n,0);
+        string ans="";
+        for(int i=0;i<n;i++){
+            if(s[i]=='*'){
+                removed[i]=1;
+                for(int j=0;j<26;j++){
+                    if(buckets[j].size()){
+                        removed[buckets[j].back()]=1;
+                        buckets[j].pop_back();
+                        break;
+                    }
+                }
+            }
+            else{
+                buckets[s[i]-'a'].push_back(i);
+            }
+        }
+
+
+
+        for(int i=0;i<n;i++){
+            if(!removed[i]){
+                ans.push_back(s[i]);
+            }
+        }
+        return ans;
+    }
+};
