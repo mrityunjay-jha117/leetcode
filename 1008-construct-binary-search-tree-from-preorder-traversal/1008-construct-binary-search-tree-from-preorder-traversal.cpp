9c60d15e31cced3b1ddd24aef527ab311f494b6a
MRITYUNJAY JHA <158163203+mrityunjay-jha117@users.noreply.github.com>
2026-06-02 13:21:34 +0530

    Time: 0 ms (100%), Space: 16 MB (89.13%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -6,22 +6,28 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-TreeNode* func(vector<int>& arr,int &i,int bound){
-    if(i==arr.size() || arr[i]>bound){
-        return nullptr;
-    }
-    TreeNode* curr=new TreeNode(arr[i++]);
-    curr->left=func(arr,i,curr->val);
-    curr->right=func(arr,i,bound);
-    return curr;
-}
+    // my solution
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        int i=0;
-        return func(preorder,i,INT_MAX);
+        map<int, TreeNode*> mp;
+        TreeNode* root = new TreeNode(preorder[0]);
+        mp[preorder[0]] = root;
+        int n = preorder.size();
+        for (int i = 1; i < n; i++) {
+            TreeNode* temp = new TreeNode(preorder[i]);
+            if (preorder[i - 1] > preorder[i]) {
+                mp[preorder[i - 1]]->left = temp;
+            } else {
+                auto node = (--mp.lower_bound(preorder[i]))->second;
+                node->right = temp;
+            }
+            mp[preorder[i]] = temp;
+        }
+        return root;
     }
 };
